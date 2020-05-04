883cbee092c55b7a94f762d307f45a2168f16911
Sachin <sachinvjose@gmail.com>
2020-05-04 12:50:45 +0530

    Updates

---
+++ precache-manifest.cb1b6815aaf28c01d8bc1c5a8ddf6a11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51ab92909fdc9fa524178b4d5b40fbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11d3dd086a6a775e099e",
+    "url": "/static/css/main.04c1d468.chunk.css"
+  },
+  {
+    "revision": "6880c6218ee0d859765b",
+    "url": "/static/js/2.f9b65c16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9b65c16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d3dd086a6a775e099e",
+    "url": "/static/js/main.a6674599.chunk.js"
+  },
+  {
+    "revision": "4eb3c859ca94350e30cd",
+    "url": "/static/js/runtime-main.783cc3df.js"
+  }
+]);
